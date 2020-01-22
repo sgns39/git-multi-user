4dd16b67692fa3d59ddcfc7eba7e4b955992e31f
sgns39 <sgns39@gmail.com>
2020-01-22 14:52:31 +0545

    initial commit

---
+++ src/test/java/com/orange/gitworks/GitWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.gitworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
