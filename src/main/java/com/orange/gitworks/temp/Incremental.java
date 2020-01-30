4241f08721f4b1d66a9873e97294c4da93b046a1
sgns39 <sgns39@gmail.com>
2020-01-30 14:58:31 +0545

    Perry Code 1

---
+++ src/main/java/com/orange/gitworks/temp/Incremental.java
@@ -0,0 +1,18 @@
+package com.orange.gitworks.temp;
+
+public class Incremental {
+
+    public static void main(String[] args) {
+
+        int column=0;
+        column++;
+        incColumnValue(column);
+        System.out.println("column: "+ column);
+    }
+
+    private static void incColumnValue(int column){
+        column++;
+        column++;
+    }
+
+}
